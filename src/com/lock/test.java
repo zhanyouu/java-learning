00b890070a3a1508d5ed788e61ede0df98f1af21
youzhan <751642772@qq.com>
2021-12-25 23:19:55 +0800

    12-25

---
+++ src/com/lock/test.java
@@ -0,0 +1,20 @@
+package com.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class test {
+
+    private Lock lock = new ReentrantLock();
+
+    public void test(){
+        lock.lock();
+        try{
+            System.out.println(1);;
+        }catch (Exception e){
+            // ignored
+        }finally {
+            lock.unlock();
+        }
+    }
+}
