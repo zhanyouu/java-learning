8459ce62e120a687603cab883ed68f160b1fb274
zhanyou <751642772@qq.com>
2023-11-27 22:29:57 +0800

    md-update

---
+++ src/com/serializer/Main.java
@@ -0,0 +1,40 @@
+package com.serializer;
+
+
+import com.serializer.client.LiClient;
+import com.serializer.common.MsgRepository;
+import com.serializer.protocal.codec.PacketCodeC;
+import com.serializer.protocal.packet.MsgPacket;
+import com.serializer.server.ZhangServer;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+
+import static com.serializer.client.LiClient.*;
+import static com.serializer.common.MsgConstant.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ServerBootstrap serverBootstrap = ZhangServer.bootstrap();
+        ChannelFuture serverChannelFuture = ZhangServer.bind(serverBootstrap, PORT);
+        Bootstrap clientBootstrap = LiClient.bootstrap();
+        ChannelFuture clientChannelFuture = LiClient.connect(clientBootstrap, HOST, PORT, MAX_RETRY);
+        serverChannelFuture.await();
+        clientChannelFuture.await();
+        for (int i = 0; i < COUNT_LEVEL_3; i++) {
+            MsgPacket one = MsgRepository.getInstance().getZhangMsgPacket(MSG_SESSION_ONE);
+            MsgPacket two = MsgRepository.getInstance().getLiMsgPacket(MSG_SESSION_TWO);
+            MsgPacket three = MsgRepository.getInstance().getLiMsgPacket(MSG_SESSION_THREE);
+            sendMsg(ZhangServer.getChannel(HOST), one);
+            sendMsg(clientChannelFuture.channel(), two);
+            sendMsg(clientChannelFuture.channel(), three);
+        }
+    }
+
+    private static void sendMsg(Channel channel, MsgPacket packet) {
+        ByteBuf byteBuf = PacketCodeC.INSTANCE.encode(packet);
+        channel.writeAndFlush(byteBuf);
+    }
+}
